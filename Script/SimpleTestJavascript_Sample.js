8de238b7fa58e6ce7c2e0a7880e96d8d7343bfca
Mukesh <mukesh.m@telliant.net>
2021-12-15 13:15:23 +0530

    first commit

---
+++ Script/SimpleTestJavascript_Sample.js
@@ -0,0 +1,60 @@
+﻿//If you are interested in scripting, this is the SimpleTest_Sample test written in Javascript.
+
+function Main() {
+  OpenWebStore("https://services.smartbear.com/samples/TestComplete15/smartstore");
+  
+  FindProduct("Solar");
+  AddToCart();
+  VerifyProductPrice("969.0");
+  RemoveFromCart();
+  
+  CloseBrowser();
+}
+
+function OpenWebStore(url) {
+  //Launch the Internet Explorer and navigate to a webpage
+  Browsers.Item(btIExplorer).Run(url); 
+}
+
+function FindProduct(productName) {
+  Aliases.Browser.FrontPage.FrontPageSearchBox.SetText(productName);
+  Aliases.Browser.FrontPage.SearchButton.ClickButton();
+  Aliases.Browser.SearchPage.FoundProduct.Click();
+}
+
+function AddToCart() {
+  //Wait until the web page is loaded completely to ensure the next click will be processed correctly by the web page
+  Aliases.Browser.ProductPage.Wait();
+  Aliases.Browser.ProductPage.AddToCartButton.ClickButton();
+}
+
+function VerifyProductPrice(price) {
+  //Check that contentText property value contains the expected price
+  aqObject.CheckProperty(Aliases.Browser.ProductPage.Cart.Subtotal, "contentText", cmpContains, price);
+}
+
+function RemoveFromCart() {
+  Aliases.Browser.ProductPage.Cart.RemoveFromCartLink.ClickButton(); 
+}
+
+function CloseBrowser() {
+  Aliases.Browser.Close();
+}
+
+function SimpleTest_Sample_1()
+{
+  //This is a sample of what a keyword test looks like within TestComplete after a recording has been done. Please feel free to tinker with the steps and the values within them!
+  //Test case: Search for an item on the website, add to cart, validate the price of the item, close the browser.
+  //Click 'Run Test' above to execute this test.
+  Browsers.Item(btChrome).Run("https://services.smartbear.com/samples/TestComplete14/smartstore");
+  Aliases.Browser.FrontPage.FrontPageSearchBox.Click();
+  Aliases.Browser.FrontPage.FrontPageSearchBox.SetText("chronograph");
+  Aliases.Browser.FrontPage.SearchButton.ClickButton();
+  Aliases.Browser.SearchPage.FoundProduct.Click();
+  Aliases.Browser.ProductPage.Wait();
+  aqObject.CheckProperty(Aliases.Browser.ProductPage.AddToCartButton, "Visible", cmpEqual, true);
+  Aliases.Browser.ProductPage.AddToCartButton.ClickButton();
+  aqObject.CheckProperty(Aliases.Browser.ProductPage.Cart.Subtotal, "contentText", cmpEqual, "$24,110.00 excl tax");
+  Aliases.Browser.ProductPage.Cart.RemoveFromCartLink.ClickButton();
+  Aliases.Browser.Close(30000);
+}
---
+++ Script/Unit1.js
@@ -0,0 +1,36 @@
+﻿
+
+function SimpleTest_Sample()
+{
+  //This is a sample of what a keyword test looks like within TestComplete after a recording has been done. Please feel free to tinker with the steps and the values within them!
+  //Test case: Search for an item on the website, add to cart, validate the price of the item, close the browser.
+  //Click 'Run Test' above to execute this test.
+  //This step opens a browser and navigates to a webpage
+  //			
+  Browsers.Item(btChrome).Run("https://services.smartbear.com/samples/TestComplete14/smartstore");
+  //This step activates the search box
+  Aliases.Browser.FrontPage.FrontPageSearchBox.Click();
+  //This step searches for the product
+  Aliases.Browser.FrontPage.FrontPageSearchBox.SetText("chronograph");
+  //This step simulates a click on the search button
+  Aliases.Browser.FrontPage.SearchButton.ClickButton();
+  //This step selects a product on the search page
+  //
+  Aliases.Browser.SearchPage.FoundProduct.Click();
+  //Pauses the script execution until the specified Web page is downloaded or until the specified time limit elapses.
+  Aliases.Browser.ProductPage.Wait();
+  //Checks whether the 'Visible' property of the Aliases.Browser.ProductPage.AddToCartButton object equals True.
+  aqObject.CheckProperty(Aliases.Browser.ProductPage.AddToCartButton, "Visible", cmpEqual, true);
+  //Simulates one or several keypresses.
+  Aliases.Browser.ProductPage.panelContentWrapper.Keys("[Down][Down][Down][Down][Down]");
+  //This step adds the selected product to the cart
+  //
+  //
+  Aliases.Browser.ProductPage.AddToCartButton.ClickButton();
+  //This step removes the product from the cart
+  //
+  Aliases.Browser.ProductPage.Cart.RemoveFromCartLink.ClickButton();
+  //This step closes the current browser
+  //
+  Aliases.Browser.Close(30000);
+}
